d99ea1e9b6f058a8c5d350055d1950ea45f408fa
leqi-t1223 <114261168+leqi-t1223@users.noreply.github.com>
2023-05-03 23:04:14 -0400

    des

--- tornadodecimatestrailerpark/R/Format_Dataset.R
+++ tornadodecimatestrailerpark/R/Format_Dataset.R
@@ -1,3 +1,15 @@
+
+#' Format the hurdat dataset in the data directory
+#'
+#' Get a data frame with the following qualities:
+#' no storm id rows.
+#' instead of storm id rows, an id column to signify which storm id each row corresponds to.
+#' a name column for the name of the storm
+#' missing value codes replaced with NAs
+#' meaningful column names. (see documentation)
+#' numeric latitude and longitude columns, in degrees north and degrees east, respectively.
+#'
+#'
 dat <- read.csv("hurdat2-1851-2022-040723.txt", header= FALSE)
 v=which(unlist(lapply(as.numeric(dat$V1),is.na))==TRUE)
 code=dat[v,1]
@@ -10,7 +22,8 @@ data$V5= as.numeric(substr(data$V5,1,nchar(data$V5)-1))
 data$V6= -as.numeric(substr(data$V6,1,nchar(data$V6)-1))
 data[data == -999] <- NA
 data[data == -99] <- NA
-colnames(data)=c("id","name","date","time","record_identifier","status","latitude","longitude","max_wind","min_pressure","wind_radii_northeastern","wind_radii_southeastern","wind_radii_southwestern","wind_radii_northwestern","wind_radii_northeastern","wind_radii_southeastern","wind_radii_southwestern","wind_radii_northwestern","wind_radii_northeastern","wind_radii_southeastern","wind_radii_southwestern","northwestern","radius_max_wind")
+colnames(data)=c("id","name","date","time","record_identifier","status","latitude","longitude","max_wind","min_pressure","34_wind_radii_northeastern","34_wind_radii_southeastern","34_wind_radii_southwestern","34_wind_radii_northwestern","50_wind_radii_northeastern","50_wind_radii_southeastern","50_wind_radii_southwestern","50_wind_radii_northwestern","64_wind_radii_northeastern","64_wind_radii_southeastern","64_wind_radii_southwestern","64_wind_radii_northwestern","radius_max_wind")
 row.names(data) <- 1 : nrow(data)
 head(data)
 save(data, file = "hurdat.RData")
+
--- tornadodecimatestrailerpark/R/data.R
+++ tornadodecimatestrailerpark/R/data.R
@@ -1,3 +1,4 @@
+
 #' Atlantic Hurricane Database
 #'
 #' A dataset with the information of all the Atlantic hurricane since 1851.This report updates the original HURDAT documentation to
@@ -9,8 +10,24 @@
 #'     \item{name}{name of the hurricane,before 1950 referred to as “UNNAMED” }
 #'     \item{date}{year, month, day of the hurricane}
 #'     \item{time}{hours in UTC, minutes of the hurricane}
-#'     \item{record_identifier}{}
+#'     \item{record_identifier}{record identifier,options are: C,G,I,L,P,R,S,T,W}
+#'     \item{status}{status of system, options are: TD,TS,HU,EX,SD,SS,LO,WV,DB}
 #'     \item{latitude}{latitude of hurricane, in decimal degrees north}
 #'     \item{longitude}{longitude of hurricane, in decimal degrees east}
+#'     \item{max_wind}{maximum sustained wind, in knots}
+#'     \item{min_pressure}{minimum pressure, in millibars}
+#'     \item{34_wind_radii_northeastern}{34 kt wind radii maximum extent in northeastern quadrant (in nautical miles)}
+#'     \item{34_wind_radii_southeastern}{34 kt wind radii maximum extent in southeastern quadrant (in nautical miles)}
+#'     \item{34_wind_radii_southwestern}{34 kt wind radii maximum extent in southwestern quadrant (in nautical miles)}
+#'     \item{34_wind_radii_northwestern}{34 kt wind radii maximum extent in northwestern quadrant (in nautical miles)}
+#'     \item{50_wind_radii_northeastern}{50 kt wind radii maximum extent in northeastern quadrant (in nautical miles)}
+#'     \item{50_wind_radii_southeastern}{50 kt wind radii maximum extent in southeastern quadrant (in nautical miles)}
+#'     \item{50_wind_radii_southwestern}{50 kt wind radii maximum extent in southwestern quadrant (in nautical miles)}
+#'     \item{50_wind_radii_northwestern}{50 kt wind radii maximum extent in northwestern quadrant (in nautical miles)}
+#'     \item{64_wind_radii_northeastern}{64 kt wind radii maximum extent in northeastern quadrant (in nautical miles)}
+#'     \item{64_wind_radii_southeastern}{64 kt wind radii maximum extent in southeastern quadrant (in nautical miles)}
+#'     \item{64_wind_radii_southwestern}{64 kt wind radii maximum extent in southwestern quadrant (in nautical miles)}
+#'     \item{64_wind_radii_northwestern}{64 kt wind radii maximum extent in northwestern quadrant (in nautical miles)}
+#'     \item{radius_max_wind}{radius of Maximum Wind (in nautical miles)}
 #' }
 "hurdat"
