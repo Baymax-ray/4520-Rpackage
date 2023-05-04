4d1f323a2cf756ea85082ca37bb51f10ec77341e
Baymax-ray <80106800+Baymax-ray@users.noreply.github.com>
2023-05-03 22:06:18 -0400

    func1

--- tornadodecimatestrailerpark/R/StormTrack30m.R
+++ tornadodecimatestrailerpark/R/StormTrack30m.R
@@ -1,7 +1,17 @@
-#'Add a storm track
+#'Interpolate storm tracks with 30min interval
 #'
 #'A function for interpolating a storm track to 30 minute increments.
-#'New storm track is added to???
+#'If one side is NA, the added value will also be NA.
+#'Return the modified data frame.
 #'
-#'@param
-#'@return Null
+#'@param df the data frame to be modified
+#'@return the interpolated data frame
+#'@example
+#'data_path <- system.file("data", "my_data.RData", package = "tornadodecimatestrailerpark")
+#'load(data_path)
+#'new_df<-Interpolate30m(hurdat)
+#'head(new_df)
+#'@export
+Interpolate30m<-function(df){
+
+}
