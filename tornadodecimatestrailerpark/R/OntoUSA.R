f5e141cd91a69fb50bfa72f7b52ded728a09df01
Baymax-ray <80106800+Baymax-ray@users.noreply.github.com>
2023-05-05 21:38:39 -0400

    partly done func4

--- tornadodecimatestrailerpark/R/OntoUSA.R
+++ tornadodecimatestrailerpark/R/OntoUSA.R
@@ -1,11 +1,40 @@
 #'If the storm landed on USA
 #'
-#'A function for determining whether the storm made landfall in the continental United States based on its latitude and longitude values.
+#'A function for determining whether the storm made landfall in the continental United States
+#'Note:continental US includes Alaska, but excludes Hawaii and the U.S. territories such as Puerto Rico and the U.S. Virgin Islands.
 #'No use of the unreliable record identifier variable for this part.
 #'Return true if it landed in the continental United States, false otherwise.
 #'
-#'@param name
+#'@param storm_data data frame of storm data
+#'@param storm_id id of this storm
 #'@return true or false
-#'
-#'
-#'
+#'@example
+#'IfLandfall<-storm_US_landfall(data,"AL122007")
+#'@export
+
+storm_US_landfall <- function(storm_data,storm_id) {
+  # Extract the data
+  storm_subset <- storm_data[storm_data$id == storm_id, ]
+
+  # Get US boundaries from the maps package
+  us_map <- map("usa", fill = TRUE, plot = FALSE,region="main")
+
+  # Convert the US boundaries to a data frame
+  us_boundary <- data.frame(x = us_map$x, y = us_map$y)
+
+  # Loop through each row of the storm_subset data frame
+  for (i in 1:nrow(storm_subset)) {
+    latitude <- storm_subset$latitude[i]
+    longitude <- storm_subset$longitude[i]
+
+    # Check if the storm's latitude and longitude are within the US boundaries
+    in_us <- point.in.polygon(point.x = longitude, point.y = latitude, pol.x = us_boundary$x, pol.y = us_boundary$y)
+    # If the storm made landfall in the continental US, return TRUE
+    if (in_us == 1) {
+      return(TRUE)
+    }
+  }
+
+  # If the loop completes without finding a landfall, return FALSE
+  return(FALSE)
+}
