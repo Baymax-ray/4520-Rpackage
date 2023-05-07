299450ab023d36ca48b8d3e67c277336cf6d4e4b
leqi-t1223 <114261168+leqi-t1223@users.noreply.github.com>
2023-05-07 14:12:02 -0400

    i

--- tornadodecimatestrailerpark/tests/testthat/test_interpolate.R
+++ tornadodecimatestrailerpark/tests/testthat/test_interpolate.R
@@ -8,6 +8,6 @@ test_that( "interpolate_storm_track_30m function looks ok", {
     interpolated$date[2],'18510625'
   )
   expect_equal(
-    interpolated$time[2],'0030'
+    interpolated$time[2],"0030"
   )
 })
